289d8b7e215ba86a5eaacca2c215de9153324ecb
Guilherme Muller <gmuller@apoioprodesp.sp.gov.br>
2025-05-09 09:58:44 -0300

    Configurações deploy

--- bookhive.client/vite.config.ts
+++ bookhive.client/vite.config.ts
@@ -7,33 +7,33 @@ import child_process from 'node:child_process';
 import { env } from 'node:process';
 
 export default defineConfig(({ mode }) => {
-    const envVars = loadEnv(mode, process.cwd(), '');
+    // const envVars = loadEnv(mode, process.cwd(), '');
 
-    const baseFolder =
-        env.APPDATA !== undefined && env.APPDATA !== ''
-            ? `${env.APPDATA}/ASP.NET/https`
-            : `${env.HOME}/.aspnet/https`;
+    // const baseFolder =
+    //     env.APPDATA !== undefined && env.APPDATA !== ''
+    //         ? `${env.APPDATA}/ASP.NET/https`
+    //         : `${env.HOME}/.aspnet/https`;
 
-    const certificateName = 'bookhive.client';
-    const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
-    const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+    // const certificateName = 'bookhive.client';
+    // const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+    // const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
-    if (!fs.existsSync(baseFolder)) {
-        fs.mkdirSync(baseFolder, { recursive: true });
-    }
+    // if (!fs.existsSync(baseFolder)) {
+    //     fs.mkdirSync(baseFolder, { recursive: true });
+    // }
 
-    if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
-        if (
-            0 !==
-            child_process.spawnSync(
-                'dotnet',
-                ['dev-certs', 'https', '--export-path', certFilePath, '--format', 'Pem', '--no-password'],
-                { stdio: 'inherit' }
-            ).status
-        ) {
-            throw new Error('Could not create certificate.');
-        }
-    }
+    // if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+    //     if (
+    //         0 !==
+    //         child_process.spawnSync(
+    //             'dotnet',
+    //             ['dev-certs', 'https', '--export-path', certFilePath, '--format', 'Pem', '--no-password'],
+    //             { stdio: 'inherit' }
+    //         ).status
+    //     ) {
+    //         throw new Error('Could not create certificate.');
+    //     }
+    // }
 
     const target = env.VITE_BACKEND_URL || 'http://localhost:5135';
 
@@ -53,10 +53,11 @@ export default defineConfig(({ mode }) => {
               }
             },
             port: parseInt(env.DEV_SERVER_PORT || '50253'),
-            https: {
-              key: fs.readFileSync(keyFilePath),
-              cert: fs.readFileSync(certFilePath),
-            }
+            https: false
+            // https: {
+            //   key: fs.readFileSync(keyFilePath),
+            //   cert: fs.readFileSync(certFilePath),
+            // }
         },          
     };
 });
